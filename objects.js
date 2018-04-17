9deaf7ac1f9c79216ff795307bc65b28f68c608f
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-17 03:09:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {galantis:'hunter'};
 function updatePlaylist(playlist, artistName,songTitle) {
   playlist[artistName] = songTitle
+  console.log(playlist)
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
