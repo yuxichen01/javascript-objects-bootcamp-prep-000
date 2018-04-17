7ba68dc004d40ea59965702236cdea62ee8e96bb
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-17 02:41:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,6 @@ function updatePlaylist(playlist, artistName,songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  
+  delete playlist.artistName;
+  return playlist
 }
